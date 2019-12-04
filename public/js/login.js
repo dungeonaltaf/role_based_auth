d81842dd6efc904283d5fae07dfaa727af954cb0
Altaf hussain <altafsofficial01@gmail.com>
2019-12-04 09:42:40 +0530

    Update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -168,7 +168,7 @@ $(document).ready(function(){
                   }
                   }).done(function (response) {
                     var res = document.createElement("p");
-                   res.innerText = JSON.parse(response.responseText);
+                   res.innerHTML = response;
                    document.getElementById('res_server_div').appendChild(res);
                   console.log(response);
                   });
